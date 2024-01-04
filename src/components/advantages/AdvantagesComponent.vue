3c4a0dcfb5bd49c075b9e07e5d13713d6b204eb3
mudmykola <mykola.mud@gmail.com>
2024-01-04 23:07:26 +0200

    YPP: "Added a list of cards"

---
+++ src/components/advantages/AdvantagesComponent.vue
@@ -0,0 +1,9 @@
+<script setup>
+import AdvantagesListComponent from '@/components/advantages/AdvantagesListComponent.vue'
+</script>
+
+<template>
+  <div class="advantages">
+    <advantages-list-component />
+  </div>
+</template>
--- src/main-content/MainContentComponent.vue
+++ src/main-content/MainContentComponent.vue
@@ -1,9 +1,11 @@
 <script setup>
 import HeaderComponent from '@/components/header/HeaderComponent.vue'
+import AdvantagesComponent from '@/components/advantages/AdvantagesComponent.vue'
 </script>
 
 <template>
-<div>
-<header-component/>
-</div>
+  <div>
+    <header-component />
+    <advantages-component />
+  </div>
 </template>
