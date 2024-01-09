33d690a2245db4f9cebf2861c203084162e3d272
mudmykola <mykola.mud@gmail.com>
2024-01-09 08:12:40 +0200

    YPP: "Added a list of symptom"

---
+++ src/components/signs/SignsItemListComponent.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { defineProps } from 'vue'
+
+const props = defineProps(['items'])
+</script>
+
+<template>
+  <ul class="grid grid-cols-3 gap-[2rem] mt-[3.125rem]">
+    <li
+      class="flex justify-center py-[0.625rem] px-[1.25rem] border-2 border-c103 w-full rounded-[0.625rem] text-[1.563rem] hover:bg-c101 hover:text-default cursor-pointer transition-transform duration-500 ease-in-out transform hover:scale-105 hover:border-default"
+      v-for="item in items"
+      :key="item">
+      {{ $t(item) }}
+    </li>
+  </ul>
+</template>
--- src/main-content/MainContentComponent.vue
+++ src/main-content/MainContentComponent.vue
@@ -1,11 +1,13 @@
 <script setup>
 import HeaderComponent from '@/components/header/HeaderComponent.vue'
 import AdvantagesComponent from '@/components/advantages/AdvantagesComponent.vue'
+import SignsComponent from '@/components/signs/SignsComponent.vue'
 </script>
 
 <template>
   <div>
     <header-component />
     <advantages-component />
+    <signs-component />
   </div>
 </template>
