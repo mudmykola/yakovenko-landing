0e604988b943eb3da5ae0f5749d6c9a12857aacb
mudmykola <mykola.mud@gmail.com>
2024-02-04 20:18:58 +0200

    Replaced the logic for passing colors to the list

--- src/components/solving/SolvingListComponent.vue
+++ src/components/solving/SolvingListComponent.vue
@@ -6,7 +6,7 @@ import solvingItems from '@/components/solving/solvingItems.js'
 <template>
   <div>
     <div class="rounded-[0.938rem]" v-for="(item, index) in solvingItems" :key="index"
-         :class="['solving-list', `bg-${item.background}`, `text-${item.color}`, index !== solvingItems.length - 1 ? 'mb-[1.75rem]' : '']"
+         :class="['solving-list', `${item.background}`, `text-${item.color}`, index !== solvingItems.length - 1 ? 'mb-[1.75rem]': '']"
          :style="{ width: item.length * 10 + 'px' }">
       <p class="py-[1.563rem] px-[2.625rem] text-[1.3rem]">
         {{ $t(item.text) }}
